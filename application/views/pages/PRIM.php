554648b64485c806e796b0cd110337a12d467e2d
Imam Arief Putrajaya <ice.codename@gmail.com>
2020-10-16 14:17:35 +0800

    update 16 oktober
    -force https
    -add content on PRIM page

--- application/views/pages/PRIM.php
+++ application/views/pages/PRIM.php
@@ -15,17 +15,41 @@
         <div class="col-md-12 col-lg-12 text-center text-md-center mb-4 mb-md-0">
             <h3 class="mb-0 font-weight-bold text-center black-text">DATA Project PRIM</h3>
         </div>
-    </div>
+    </div>    
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TVdEcXJOUjNkRUU/view" target="_blank">Paket PRIM 2017</a>
+    <br>
     <a href="https://drive.google.com/file/d/1FyaJ7zyz5nU1qXw9kIm05FNuKkKs0wSt/preview" target="_blank">Daftar Ruas P3,P4,P5 PRIM 2018</a>    
     <br>
     <a href="https://drive.google.com/file/d/1MpJ6s2LqhSzv6f30PHVaueOwsq-Bo132/preview" target="_blank">Streaking Map Swakelola PRIM 2018</a>
     <h4>Paket P1 (Montong Buwuh - Lendang Bajur)</h4>
     <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-Nmx0cVNBdTJZRDA/preview" target="_blank">Peta</a>
     <br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-aDlicXQ0QVlYTlk/view" target="_blank">Daftar Ruas</a>
+    <br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TTdkbTdNa3hBWGs/view" target="_blank">Daftar Kuantitas dan Harga</a>
+    <br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-enRlNlpxcEk3RW8/view" target="_blank">Gambar Rencana Kerja</a>
     <h4>Paket PII (Narmada - Batu Kumbung)</h4>
     <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TnlCSDRSUTZSUms/preview" target="_blank">Daftar Ruas</a>    
     <br>
-    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TmIwMG9OU0lpUDQ/preview" target="_blank">Daftar Kuantitas dan Harga</a>    
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TmIwMG9OU0lpUDQ/preview" target="_blank">Daftar Kuantitas dan Harga</a>
+    <br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-MHotU1NjU3BleFk/view" target="_blank">Gambar Rencana Kerja</a>
+    <br>
+    <a href="https://drive.google.com/file/d/0B2K-m15DOxq_ZVJkOHdHdHY4UVE/view?usp=sharing" target="_blank">40 Matadata CoST</a>
+    <h4>Paket PIII (Sayang-Sayang - Gunungsari)</h4>
+    <a href="https://drive.google.com/file/d/12ip5TSOeOcgZfQIqwOlCL2fqH9fuYFJQ/preview" target="_blank">40 Matadata CoST (25 April 2018)</a>    
+    <br>
+    <a href="https://drive.google.com/file/d/1k36iDxDjdrcpB_HMHWadaCkNgPhts1g8/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a>
+    <h4>Paket PIV (Kediri - Dasan Bagu)</h4>
+    <a href="https://drive.google.com/file/d/1IBxp3EDEfHGDT0hhOnrhD1i228XfPIRi/preview" target="_blank">40 Matadata CoST (25 April 2018)</a>    
+    <br>
+    <a href="https://drive.google.com/file/d/1X_xDIePb5Hs92SzN_NziDSWsdc8kmCuK/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a>
+    <h4>Paket PV (Kumbung - Kuripan)</h4>
+    <a href="https://drive.google.com/file/d/1PNcYv-W9eZ_l1PnyU7bZ0DeHy-2GLEIr/preview" target="_blank">40 Matadata CoST (25 April 2018)</a>    
+    <br>
+    <a href="https://drive.google.com/file/d/1Z8ODxjRP6HIfFoCUDqrfWhXQq3o_y45J/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a>
+   
 </div>
 <div class="col-md-6 mb-4">
     <div class="py-4 d-flex align-items-center">
@@ -45,7 +69,6 @@
     <a href="https://drive.google.com/file/d/194s4oXIsiOGlqUntA5W5JeHkNDnavSv0/preview" target="_blank">Data Gender Dalam PRIM 2017</a>
     
 </div>              
-
 
 </section>
             <!--Section: Gallery-->
