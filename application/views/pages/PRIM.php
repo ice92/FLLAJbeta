40b1a8eb0059020e5142caeede7dfdcb79b3430f
Imam Arief Putrajaya <ice.codename@gmail.com>
2020-11-10 14:51:21 +0800

    Update PRIM.php

--- application/views/pages/PRIM.php
+++ application/views/pages/PRIM.php
@@ -16,40 +16,113 @@
             <h3 class="mb-0 font-weight-bold text-center black-text">DATA Project PRIM</h3>
         </div>
     </div>    
-    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TVdEcXJOUjNkRUU/view" target="_blank">Paket PRIM 2017</a>
-    <br>
-    <a href="https://drive.google.com/file/d/1FyaJ7zyz5nU1qXw9kIm05FNuKkKs0wSt/preview" target="_blank">Daftar Ruas P3,P4,P5 PRIM 2018</a>    
-    <br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TVdEcXJOUjNkRUU/view" target="_blank">Paket PRIM 2017</a><br>
+    <a href="https://drive.google.com/file/d/1FyaJ7zyz5nU1qXw9kIm05FNuKkKs0wSt/preview" target="_blank">Daftar Ruas P3,P4,P5 PRIM 2018</a><br>
     <a href="https://drive.google.com/file/d/1MpJ6s2LqhSzv6f30PHVaueOwsq-Bo132/preview" target="_blank">Streaking Map Swakelola PRIM 2018</a>
     <h4>Paket P1 (Montong Buwuh - Lendang Bajur)</h4>
-    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-Nmx0cVNBdTJZRDA/preview" target="_blank">Peta</a>
-    <br>
-    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-aDlicXQ0QVlYTlk/view" target="_blank">Daftar Ruas</a>
-    <br>
-    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TTdkbTdNa3hBWGs/view" target="_blank">Daftar Kuantitas dan Harga</a>
-    <br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-Nmx0cVNBdTJZRDA/preview" target="_blank">Peta</a><br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-aDlicXQ0QVlYTlk/view" target="_blank">Daftar Ruas</a><br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TTdkbTdNa3hBWGs/view" target="_blank">Daftar Kuantitas dan Harga</a><br>
     <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-enRlNlpxcEk3RW8/view" target="_blank">Gambar Rencana Kerja</a>
     <h4>Paket PII (Narmada - Batu Kumbung)</h4>
-    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TnlCSDRSUTZSUms/preview" target="_blank">Daftar Ruas</a>    
-    <br>
-    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TmIwMG9OU0lpUDQ/preview" target="_blank">Daftar Kuantitas dan Harga</a>
-    <br>
-    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-MHotU1NjU3BleFk/view" target="_blank">Gambar Rencana Kerja</a>
-    <br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TnlCSDRSUTZSUms/preview" target="_blank">Daftar Ruas</a><br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-TmIwMG9OU0lpUDQ/preview" target="_blank">Daftar Kuantitas dan Harga</a><br>
+    <a href="https://drive.google.com/file/d/0B_pm8b-p7iJ-MHotU1NjU3BleFk/view" target="_blank">Gambar Rencana Kerja</a><br>
     <a href="https://drive.google.com/file/d/0B2K-m15DOxq_ZVJkOHdHdHY4UVE/view?usp=sharing" target="_blank">40 Matadata CoST</a>
     <h4>Paket PIII (Sayang-Sayang - Gunungsari)</h4>
-    <a href="https://drive.google.com/file/d/12ip5TSOeOcgZfQIqwOlCL2fqH9fuYFJQ/preview" target="_blank">40 Matadata CoST (25 April 2018)</a>    
-    <br>
-    <a href="https://drive.google.com/file/d/1k36iDxDjdrcpB_HMHWadaCkNgPhts1g8/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a>
+    <a href="https://drive.google.com/file/d/12ip5TSOeOcgZfQIqwOlCL2fqH9fuYFJQ/preview" target="_blank">40 Matadata CoST (25 April 2018)</a><br>    
+    <a href="https://drive.google.com/file/d/1k36iDxDjdrcpB_HMHWadaCkNgPhts1g8/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a><br>    
+    <a href="https://drive.google.com/file/d/1J-wgAFzqrxxrQSstiZMhXoXvkb14lqmd/preview" target="_blank">40 Matadata CoST (31 Mei 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1nqM4I4Ih5lPclCXSiydsAgWlWaJhea2R/preview" target="_blank">40 Matadata CoST (7 Juni 2018)</a><br>
+    <a href="https://drive.google.com/file/d/14_T-7SttUO99QGZrkVWmbPs0E_4hOHuw/preview" target="_blank">40 Matadata CoST (31 Juli 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1hT09TTs9HUCU3MIUmJoHOBTsLl6om04a/preview" target="_blank">40 Matadata CoST (31 Agustus 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1EHik69PF8gJmH4JuvJGT_Raw8zqBdEbb/preview" target="_blank">40 Matadata CoST (23 September 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1MVurStrJuwid5jk0ybg2QKnNasqa1CqK/preview" target="_blank">40 Matadata CoST (23 Oktober 2018)</a>
+    
     <h4>Paket PIV (Kediri - Dasan Bagu)</h4>
-    <a href="https://drive.google.com/file/d/1IBxp3EDEfHGDT0hhOnrhD1i228XfPIRi/preview" target="_blank">40 Matadata CoST (25 April 2018)</a>    
-    <br>
-    <a href="https://drive.google.com/file/d/1X_xDIePb5Hs92SzN_NziDSWsdc8kmCuK/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a>
+    <a href="https://drive.google.com/file/d/1IBxp3EDEfHGDT0hhOnrhD1i228XfPIRi/preview" target="_blank">40 Matadata CoST (25 April 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1X_xDIePb5Hs92SzN_NziDSWsdc8kmCuK/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1ZRUB-gBSu-ztxnOO7BginIC9uiXtDaqE/preview" target="_blank">40 Matadata CoST (31 Mei 2018)</a><br>
+    <a href="https://drive.google.com/file/d/11orfFNx8OQ-ms8xnJOqQS7VNESmqBzhx/preview" target="_blank">40 Matadata CoST (7 Juni 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1iL1ut4hHt5bE2kQrm9N_5VDL5O4ecEPm/preview" target="_blank">40 Matadata CoST (31 Juli 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1zfIsK3zgTaa5N_ERbeEV0f0DUlPzbHQm/preview" target="_blank">40 Matadata CoST (31 Agustus 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1Dg0oIwF5rri5nCPXQebdQd9kpJvp01Tj/preview" target="_blank">40 Matadata CoST (23 September 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1vEZp2VtxfTQ9uolfwfuFq08-ADIdQAz3/preview" target="_blank">40 Matadata CoST (23 Oktober 2018)</a>
+    
     <h4>Paket PV (Kumbung - Kuripan)</h4>
-    <a href="https://drive.google.com/file/d/1PNcYv-W9eZ_l1PnyU7bZ0DeHy-2GLEIr/preview" target="_blank">40 Matadata CoST (25 April 2018)</a>    
-    <br>
-    <a href="https://drive.google.com/file/d/1Z8ODxjRP6HIfFoCUDqrfWhXQq3o_y45J/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a>
+    <a href="https://drive.google.com/file/d/1PNcYv-W9eZ_l1PnyU7bZ0DeHy-2GLEIr/preview" target="_blank">40 Matadata CoST (25 April 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1Z8ODxjRP6HIfFoCUDqrfWhXQq3o_y45J/preview" target="_blank">40 Matadata CoST (7 Mei 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1RAgW1-jOcV_1OZ2qn8rUwq7FyGrMRjUd/preview" target="_blank">40 Matadata CoST (31 Mei 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1MgU9XwQVH3Sg_BPwg6HssSIci0qnwazk/preview" target="_blank">40 Matadata CoST (7 Juni 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1zrUY9IJewvHZAlmeeqMaFzlOjqHGFee6/preview" target="_blank">40 Matadata CoST (31 Juli 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1fLPIcZdx2cuOicIZE7DF8zULrVy1qSYO/preview" target="_blank">40 Matadata CoST (31 Agustus 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1qLtrjUgNz5qFkrs2ba5gkDbi3zHD-JEj/preview" target="_blank">40 Matadata CoST (23 September 2018)</a><br>
+    <a href="https://drive.google.com/file/d/1_-Cfa-XTgT5LokgA9Q2bwzHK90erzx9U/preview" target="_blank">40 Matadata CoST (23 Oktober 2018)</a>
    
+    <h4>Paket PVI (Meninting - Midang)</h4>
+    <a href="https://drive.google.com/file/d/1ZbRJy9QZli_Qa5mkRAS4OJgtzy9p1I_JglRGcsoLaSE/preview" target="_blank">40 Matadata CoST (Januari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1pa2b-mO-S-AhWQk1IAkhGZ49XBgktIyzRBZXuhjlTrY/preview" target="_blank">40 Matadata CoST (Februari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1yAeXIiVJ2bIMyUl-fkUb_lU3f1W1LWOi/preview" target="_blank">40 Matadata CoST (Maret 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1zCi9XqVNxP9AUG0IOrqB06WM4IGiKeaO/preview" target="_blank">40 Matadata CoST (April 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1tY39at_zg0YeMnuIcuoBbNRZd78NdgxE/preview" target="_blank">40 Matadata CoST (Mei 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1gmX2P_Ea-hsVUIWqeXUNoBv1hk7rvcMJ/preview" target="_blank">40 Matadata CoST (Juni 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1MSBbA7gaFD0FdcGz5zf1vVC2XCKxGLLm/preview" target="_blank">40 Matadata CoST (Juli 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1gBHH5GKg-pvELw481bgacIBAg5qM2RJR/preview" target="_blank">40 Matadata CoST (Agustus 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1Hj6WeO4j-ys8U-wxqzXDrKKYQx4mswUV/preview" target="_blank">40 Matadata CoST (September 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1J3q6ULgPwyPEv7F-t00dPJeitVrRchdF/preview" target="_blank">40 Matadata CoST (Oktober 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1X7QUIrtEaGBVbsRDHKprJknquwPFSYV_/preview" target="_blank">40 Matadata CoST (November 2019)</a>
+    
+    <h4>Paket PVII (Dasan Geres - Buntage)</h4>
+    <a href="https://drive.google.com/file/d/1A3Y9_ztjZa-kN6tlSuYYaYtXQ75pNv1RVQqDMWQd3X4/preview" target="_blank">40 Matadata CoST (Januari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1yYiscpjKaRoKI9XDOOrjXCI3Y-pT4upkTvHaNF4xwO8/preview" target="_blank">40 Matadata CoST (Februari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/17NiP2db23sOK9h6AW8Oq3mI3wGfuy6gB/preview" target="_blank">40 Matadata CoST (Maret 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1u1qUliQ-4PVGari23cmX8horX9bWpvZc/preview" target="_blank">40 Matadata CoST (April 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1K0n4F3yqI1hT0EIl3oeA6-7ls6edhic-/preview" target="_blank">40 Matadata CoST (Mei 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1K1YMXIgQVoNKoTazXuW9K9JmMYYyZv-p/preview" target="_blank">40 Matadata CoST (Juni 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1E2LwrFGyBI9XhEfvCIRRyLf1NlU2qN8Y/preview" target="_blank">40 Matadata CoST (Juli 2019)</a><br>
+    <a href="https://drive.google.com/file/d/11iwjS9HW7iwbjfn-XwDV4LWWoEYkvAfq/preview" target="_blank">40 Matadata CoST (Agustus 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1eUJvGQVCx_oZ6g9Md4tVVL0V3dcjznXo/preview" target="_blank">40 Matadata CoST (September 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1J1yxEyXmdi3GSRuAwqKGMELSxz0SCalC/preview" target="_blank">40 Matadata CoST (Oktober 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1BnbeefRQ7UwIz1dSnlED4f3bp0Jd5CNY/preview" target="_blank">40 Matadata CoST (November 2019)</a>
+    
+    <h4>Paket PVIII (Dasan Tereng - Sembung)</h4>
+    <a href="https://drive.google.com/file/d/1KUtNLRHmzzBRzPNdWsMe_yE5PvmJ-5RPSrotefHP1-g/preview" target="_blank">40 Matadata CoST (Januari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/19thWs3yGTLbS7bCmcEUBwVDAYjaFJU3KdEziO0mcGmU/preview" target="_blank">40 Matadata CoST (Februari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1Of4wyLUs9Sp52Obje6Ff8phD5HhzeLvJ/preview" target="_blank">40 Matadata CoST (Maret 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1X06-0WuQVes8XJuCKaX6sEQ0iznQipoo/preview" target="_blank">40 Matadata CoST (April 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1CehkB1pIomjjufTkPFm1Ijc4XhiShXoZ/preview" target="_blank">40 Matadata CoST (Mei 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1zjkqRcA3b9g0_XKVjdoARsvOscCUQdA-/preview" target="_blank">40 Matadata CoST (Juni 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1gxxUM49RrM1zb4XahBlD02oVqVd-lxGO/preview" target="_blank">40 Matadata CoST (Juli 2019)</a><br>
+    <a href="https://drive.google.com/file/d/17ADtMlnpSZZ72f4la-cuIIaV5nvMLwJ9/preview" target="_blank">40 Matadata CoST (Agustus 2019)</a><br>
+    <a href="https://drive.google.com/file/d/18y7NZOXOM-xTDVtO3J4NsHoEKLfGWeka/preview" target="_blank">40 Matadata CoST (September 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1QMCWyWnAWMeD9j_Y9CavY0jzxX3x1v_K/preview" target="_blank">40 Matadata CoST (Oktober 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1i0gV2ZcSEr-U_nhTZBaOGiv834K67XwL/preview" target="_blank">40 Matadata CoST (November 2019)</a>
+    
+    <h4>Paket IX (Gerung - Bantir)</h4>
+    <a href="https://drive.google.com/file/d/1Fnm6OEqUJ1l85o9W3LdfOjLGoiXc7Ag9h_jA2BrrWrA/preview" target="_blank">40 Matadata CoST (Januari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1_fQUqp6hvxGWkeZehTzMTHq8STciVRQHpaaxWv9QpwQ/preview" target="_blank">40 Matadata CoST (Februari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1O_U4l5izhqlOnO_kjEZHAiSBoyfkLKk9/preview" target="_blank">40 Matadata CoST (Maret 2019)</a><br>
+    <a href="https://drive.google.com/file/d/14ZwcPzKxHYrZPu-ft79si_7PJOG2lZB2/preview" target="_blank">40 Matadata CoST (April 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1vHXX6xcI48_lRozjGUwcLXA70VcozXsl/preview" target="_blank">40 Matadata CoST (Mei 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1y-NM6hjyxpLZLLkclkGBgUzlAdURs1s5/preview" target="_blank">40 Matadata CoST (Juni 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1OTaGN7TiajX0biCbB5Yyz3XkpRnmn3Rq/preview" target="_blank">40 Matadata CoST (Juli 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1mnZz3qpEBYo0GMTcImOktK5BrZvTD0T2/preview" target="_blank">40 Matadata CoST (Agustus 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1bVNb-vxJlHVJvc4PSEQ_bv997FP-oJBC/preview" target="_blank">40 Matadata CoST (September 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1lry90xCaGv9Guct2-aXfIbCOJg2mEDFV/preview" target="_blank">40 Matadata CoST (Oktober 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1Vzdy4pl29FBsuCSe7ZCd00eUN0k-hWzq/preview" target="_blank">40 Matadata CoST (November 2019)</a>
+    
+    <h4>Paket X (Keru - Suranadi)</h4>
+    <a href="https://drive.google.com/file/d/1F3ioJOMQ-zorYe5pfjhM858kZgf2b8zn4NAMYVrGtE8/preview" target="_blank">40 Matadata CoST (Januari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1yl8Gcq4zqSrnBJfDDLFIDfnaT_oonBx2hnb9O-9NkHk/preview" target="_blank">40 Matadata CoST (Februari 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1C7GuxX8liLHreY0uKpqR0WIsLZg3tMaw/preview" target="_blank">40 Matadata CoST (Maret 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1792vmp7VqCQaCf1Pvd6Gm0E0BHL9sZPW/preview" target="_blank">40 Matadata CoST (April 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1bUA6ujSghBmyoO6dbp_woSRYoC9YzjN9/preview" target="_blank">40 Matadata CoST (Mei 2019)</a><br>
+    <a href="https://drive.google.com/file/d/15-zZQmwwZT3JR35tsjvT_mzGR3FZHezj/preview" target="_blank">40 Matadata CoST (Juni 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1a7ZDBE28IeACWCcxyGBXk69BW0hDllFZ/preview" target="_blank">40 Matadata CoST (Juli 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1NvKMHf4-lARN5B1aR5x2KHnmtfveoa2U/preview" target="_blank">40 Matadata CoST (Agustus 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1cYdip8x61yn9uEzkYF39eRTpcVAqD62r/preview" target="_blank">40 Matadata CoST (September 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1YsgEqToZZ7THR_j6JICemtnZ_IPbOWR9/preview" target="_blank">40 Matadata CoST (Oktober 2019)</a><br>
+    <a href="https://drive.google.com/file/d/1MFWpnVIuX5_R3gj4Vb9qJXuZdRScr_bd/preview" target="_blank">40 Matadata CoST (November 2019)</a>
 </div>
 <div class="col-md-6 mb-4">
     <div class="py-4 d-flex align-items-center">
